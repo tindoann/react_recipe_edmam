4cdb8723ae1b0b228069cc97ba8e920ebd6e8bcc
tindoann <tindoann@gamil.com>
2020-05-26 17:50:02 -0400

    added click counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const APP_ID = '03d93cf5'; 
+  const APP_KEY = '9948c5638b7f38a81cc4f0132b996751'
+  const exampleReq = 
+  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}`
 
-export default App;
+  const [counter, setCounter] = useState(0); 
+
+  useEffect(() => {
+    console.log('Effect has been run'); 
+  }); 
+
+  return (
+    <div className='App'>
+      <form className='search-form'>
+        <input className='search-bar' type="text" />
+        <button className='search-button' type='submit'>
+          Search
+        </button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+    </div>
+  ); 
+}; 
+
+export default App; 
