1d242da36a7e983aca65d4b54f6f9b5185ddd843
tindoann <tindoann@gamil.com>
2020-05-26 23:52:17 -0400

    added query function

--- src/App.js
+++ src/App.js
@@ -8,23 +8,39 @@ const App = () => {
 
   // set that state and setState
   const [recipes, setRecipes] = useState([]); 
+  const [search, setSearch] = useState('');
+  
+  // query after we set the search 
+  const [query, setQuery] = useState('chicken'); 
 
   useEffect(() => {
     getRecipes(); 
-  }, []); 
+  }, [query]); 
 
   const getRecipes = async () => {
     const response = await fetch(  
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json(); 
     setRecipes(data.hits); 
+    console.log(data.hits); 
+  }
+
+  // update the event search value when user enters an item
+  const updateSearch = e => {
+    setSearch(e.target.value); 
+  }
+
+  // when when submit the getsearch, we get the the query afterwards
+  const getSearch = e => {
+    e.preventDefault(); 
+    setQuery(search);
   }
 
   return (
     <div className='App'>
-      <form className='search-form'>
-        <input className='search-bar' type="text" />
+      <form className='search-form' onSubmit={getSearch}>
+        <input className='search-bar' type="text" value={search} onChange={updateSearch} />
         <button className='search-button' type='submit'>
           Search
         </button>
